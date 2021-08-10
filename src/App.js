8bc57212a5b709076acc248381db0ea6b5d44ed3
Satyarth2000 <52520522+Satyarth2000@users.noreply.github.com>
2021-08-10 15:48:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { HashRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <HashRouter>
+    <HashRouter basename = {process.env.satyarth2000.github.io/CRUD-React-App/>
       <NavBar />
       <Switch>
         <Route exact path="/" component={Crudapp} />
